8b0822883de7fc34ff1b6562fdc1cdedc2c0de28
wouterfm <wouterfm@gmail.com>
2022-07-10 23:12:25 +0200

    Update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,39 @@
 <script>
-import { ref } from 'vue'
-export default {
-  props: {
-    pageTitle: String
-  },
-  setup() {
-    return {
-    }
-  },
- 
- 
-}
+  import {
+    ref
+  } from 'vue'
+  export default {
+    setup() {
+      let x = "Moi"
+        x = "Moi eem"
+      const asd = () => {
+        x = "Moi eem"
+      }
+      return {
+        asd,
+        x
+      }
+    },
+
+
+  }
 </script>
 
 <template>
-  <header class="relative z-15">
-  <!-- Mobile header -->
-  <div class="flex sm:hidden justify-center">
-        <div></div>
-    <div><img class="h-20 sm:h-20" src="../assets/brand_fs_bw-2022 (1).png"></div>
-        <div></div>
-  </div>
-<!-- Default header -->
-    <div class="hidden sm:flex justify-left">
-        <div></div>
-    <div><img class="h-20 sm:h-20" src="../assets/brand_fs_bw-2022 (1).png"></div>
-        <div></div>
-  </div>
-      </header>
-      <!-- /<router-link to="/welcome" class="text-gray-800 hover:text-indigo-500 font-light">No dash</router-link> -->
+  <header class="relative z-15 p-2">
+    <!-- Mobile header -->
+    <div class="flex sm:hidden justify-evenly items-center">
+      <div><button @click="asd()" class="btn btn--primary">DEMO</button></div>
+      <div><img class="w-56 h-auto" src="../assets/brand_fs_bw-2022 (1).png"></div>
+      <div><button @click="asd()" class="btn btn--primary">DEMO</button></div>
+    </div>
+    <!-- Default header -->
+    <div class="hidden sm:flex justify-between items-center">
+       /<router-link to="/welcome" class="text-white hover:text-indigo-500 font-light">No dash</router-link>
+         /<router-link to="/welcome" class="text-white hover:text-indigo-500 font-light">No dash</router-link>
+           /<router-link to="/welcome" class="text-white hover:text-indigo-500 font-light">No dash</router-link>
+      <div><img class="relative max-w-full w-56 h-auto" src="../assets/brand_fs_bw-2022 (1).png"></div>
+       /<router-link to="/welcome" class="text-white hover:text-indigo-500 font-light">No dash</router-link>
+    </div>
+  </header>
 </template>
