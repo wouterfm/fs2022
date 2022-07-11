2897a6c62fb2833642f9b4bff74f9230c76d3d9a
wouterfm <wouterfm@gmail.com>
2022-07-11 16:05:08 +0200

    Update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,54 @@
 <script>
-  import {
-    ref
-  } from 'vue'
-  export default {
-    setup() {
-      let x = "Moi"
-        x = "Moi eem"
-      const asd = () => {
-        x = "Moi eem"
-      }
-      return {
-        asd,
-        x
-      }
-    },
+   import {
+     ref
+   } from 'vue'
+   export default {
+     setup() {
+      let menu = ref(false)
+      const changeMenu = () => {
+        if (menu.value) {
+          menu.value = false
+        } else {
+          menu.value = true
+        }
 
-
-  }
+      }
+       return {
+        menu,
+        changeMenu
+       }
+     },
+   
+   
+   }
 </script>
-
 <template>
-  <header class="relative z-15 p-2">
-    <!-- Mobile header -->
-    <div class="flex sm:hidden justify-evenly items-center">
-      <div><button @click="asd()" class="btn btn--primary">DEMO</button></div>
-      <div><img class="w-56 h-auto" src="../assets/brand_fs_bw-2022 (1).png"></div>
-      <div><button @click="asd()" class="btn btn--primary">DEMO</button></div>
-    </div>
-    <!-- Default header -->
-    <div class="hidden sm:flex justify-between items-center">
-       /<router-link to="/welcome" class="text-white hover:text-indigo-500 font-light">No dash</router-link>
-         /<router-link to="/welcome" class="text-white hover:text-indigo-500 font-light">No dash</router-link>
-           /<router-link to="/welcome" class="text-white hover:text-indigo-500 font-light">No dash</router-link>
-      <div><img class="relative max-w-full w-56 h-auto" src="../assets/brand_fs_bw-2022 (1).png"></div>
-       /<router-link to="/welcome" class="text-white hover:text-indigo-500 font-light">No dash</router-link>
-    </div>
-  </header>
+   <div v-if="menu" class="overlay w-screen h-screen block fixed z-50 p-4 mt-3 bg-black">
+    <div class="hidden lg:flex items-center justify-end px-8">
+         <div class="flex items-center">
+             <i @click="changeMenu()" class="text-white fa-solid fa-xmark text-5xl cursor-pointer hover:text-indigo-500 transition-all"></i>
+         </div>
+      </div>
+      <div class="flex flex-col mt-5 px-8">
+      <router-link to="/" class="text-white hover:text-indigo-500 font-light transition-all" >Home</router-link>
+       <router-link to="/welcome" class="text-white hover:text-indigo-500 font-light transition-all">No dash</router-link>
+     </div>
+         </div>
+
+   <header class="relative p-4 z-0">
+      <!-- Mobile header -->
+      <div class="flex lg:hidden items-center justify-between px-8">
+         <div><img class="max-w-full w-16 h-auto" src="../assets/brand_fs_bw-2022 (1).png"></div>
+         <div class="flex items-center">
+            <i @click="changeMenu()" class="text-white fa-solid fa-bars text-4xl cursor-pointer hover:text-indigo-500 transition-all"></i>
+         </div>
+      </div>
+      <!-- Default header -->
+    <div class="hidden lg:flex items-center justify-between px-8">
+         <div><img class="max-w-full w-16 h-auto" src="../assets/brand_fs_bw-2022 (1).png"></div>
+         <div class="flex items-center">
+             <i @click="changeMenu()" class="text-white fa-solid fa-bars text-4xl cursor-pointer hover:text-indigo-500 transition-all"></i>
+         </div>
+      </div>
+   </header>
 </template>
